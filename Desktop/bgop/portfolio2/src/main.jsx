b0467162a1fa8edab8798dbc0d7c739e1d59316d
unknown <jeremie229242@gmail.com>
2026-07-01 11:26:36 +0100

    port react-config

---
+++ Desktop/bgop/portfolio2/src/main.jsx
@@ -0,0 +1,21 @@
+
+
+/*Node_modules*/
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+
+/*components*/ 
+import App from './App.jsx'
+
+/*css link*/ 
+
+import './index.css'
+import 'lenis/dist/lenis.css'
+
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
